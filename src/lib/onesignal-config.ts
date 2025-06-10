ad7b2327263739ef07c66f884983c965de81ac5d
lehan-alfanahel <ukin1solehan@gmail.com>
2025-06-10 14:13:33 +0700

    Add files via upload

---
+++ src/lib/onesignal-config.ts
@@ -0,0 +1,5 @@
+export const ONESIGNAL_CONFIG = {
+APP_ID: 'c8ac779e-241b-4903-8ed4-6766936a4fee',
+API_KEY: 'os_v2_app_zcwhphredneqhdwum5tjg2sp5zlobxnaq2oegheswznxni45eipldyel2hh5hbjrqctcbv2oy6fjs66u26ywel323msitf4r5l2u2ui',
+API_URL: 'https://onesignal.com/api/v1/notifications'
+};
